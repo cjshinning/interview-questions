a42d6b62ee8cb0f4c59a4d268c93f32683638ecb
cjshinning <784319417@qq.com>
2018-04-18 22:29:42 +0800

    回调和promise

--- webpack-wfp/src/index.js
+++ webpack-wfp/src/index.js
@@ -25,27 +25,85 @@
 // console.log(m.__proto__ === MathHandle.prototype)   //true
 
 // 动物
-class Animal {
-    constructor(name){
-        this.name = name
-    }
-    eat(){
-        console.log(this.name + ' eat')
-    }
+// class Animal {
+//     constructor(name){
+//         this.name = name
+//     }
+//     eat(){
+//         console.log(this.name + ' eat')
+//     }
+// }
+
+// // 狗
+// class Dog extends Animal {
+//     constructor(name){
+//         super(name)
+//         this.name = name
+//     }
+//     say () {
+//         console.log(this.name + ' say')
+//     }
+// }
+
+// // 绑定原型，实现继承
+// const dog = new Dog('哈士奇')
+// dog.say()   //哈士奇 say
+// dog.eat()   //哈士奇 eat
+
+// function loadImg(src, callback, fail){
+//     var img = document.createElement('img')
+//     img.onload = function(){
+//         callback(img)
+//     }
+//     img.onerror = function(){
+//         fail()
+//     }
+//     img.src = src
+//     // console.log(img)
+// }
+
+// var src = 'https://www.imooc.com/static/img/index/logo.png'
+// loadImg(src, function(img){
+//     console.log(img.width)
+// }, function(){
+//     console.log('fail')
+// })
+
+function loadImg(src){
+    const promise = new Promise(function(resolve, reject){
+        var img = document.createElement('img')
+        img.onload = function(){
+            resolve(img)
+        }
+        img.onerror = function(){
+            reject()
+        }
+        img.src = src
+    })
+    return promise
 }
 
-// 狗
-class Dog extends Animal {
-    constructor(name){
-        super(name)
-        this.name = name
-    }
-    say () {
-        console.log(this.name + ' say')
-    }
-}
+var src = 'https://www.imooc.com/static/img/index/logo.png'
+var result = loadImg(src)
+result.then(function(img){
+    console.log(img.width)
+}, function(){
+    console.log('fail')
+})
+result.then(function(img){
+    console.log(img.height)
+})
 
-// 绑定原型，实现继承
-const dog = new Dog('哈士奇')
-dog.say()   //哈士奇 say
-dog.eat()   //哈士奇 eat
+
+
+
+
+
+
+
+
+
+
+
+
+
